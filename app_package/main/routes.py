fc98e00c2191a4ff06acd54985dd1f21022d87b3
costa-rica <nrodrig1@gmail.com>
2023-05-21 18:53:40 +0200

    added endpoint for checking pending_rincon_invitations.json file and add user to pending rincons

--- app_package/main/routes.py
+++ app_package/main/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import render_template, jsonify, send_from_directory,current_app, request
+from flask import render_template, jsonify, send_from_directory,current_app, request, make_response
 import os
 import logging
 from logging.handlers import RotatingFileHandler
@@ -7,7 +7,8 @@ from logging.handlers import RotatingFileHandler
 from tr01_models import sess, Users, Rincons, RinconsPosts, UsersToRincons, \
     RinconsPostsComments, RinconsPostsLikes, RinconsPostsCommentsLikes
 from app_package.token_decorator import token_required
-
+from app_package.main.utils import create_rincon_posts_list
+import json
 
 main = Blueprint('main', __name__)
 
@@ -45,19 +46,21 @@ def rincon(current_user, rincon_id):
     
     rincon = sess.query(Rincons).filter_by(id= rincon_id).first()
 
-    posts_list = []
-    for post in rincon.posts:
-        post_dict = {}
-        post_dict["id"] = str(post.id)
-        post_dict["user_id"] = str(post.user_id)
-        post_created_by_user = sess.query(Users).filter_by(id=post.user_id).first()
-        post_dict["username"] = str(post_created_by_user.username)
-        post_dict["rincon_id"] = str(post.rincon_id)
-        post_dict["post_text"] = str(post.post_text)
-        post_dict["post_date"] = str(post.time_stamp_utc)
-        post_dict["image_file_name"] = post.image_file_name
-        post_dict["video_file_name"] = post.video_file_name
-        posts_list.append(post_dict)
+    # posts_list = []
+    # for post in rincon.posts:
+    #     post_dict = {}
+    #     post_dict["id"] = str(post.id)
+    #     post_dict["user_id"] = str(post.user_id)
+    #     post_created_by_user = sess.query(Users).filter_by(id=post.user_id).first()
+    #     post_dict["username"] = str(post_created_by_user.username)
+    #     post_dict["rincon_id"] = str(post.rincon_id)
+    #     post_dict["post_text"] = str(post.post_text)
+    #     post_dict["post_date"] = str(post.time_stamp_utc)
+    #     post_dict["image_file_name"] = post.image_file_name
+    #     post_dict["video_file_name"] = post.video_file_name
+    #     posts_list.append(post_dict)
+
+    posts_list = create_rincon_posts_list(current_user, rincon_id)
 
     logger_main.info(f"- sending rincon's post: {len(posts_list)} posts")
     logger_main.info(f"- first post is: {posts_list[0]}")
@@ -89,5 +92,63 @@ def rincon_file(current_user, file_name):
         rincon_files_db_folder_name), image_filename)
 
 
+@main.route("/check_invite_json", methods=["POST"])
+def check_invite_json():
+    logger_main.info(f"- in check_invite_json")
 
+    try:
+        request_json = request.json
+        logger_main.info("request_json:",request_json)
+    except Exception as e:
+        logger_main.info(e)
+        return make_response('Could not verify', 400, {'message' : 'httpBody data recieved not json not parse-able.'})
+
+    website_credentials = request_json.get("tu-rincon.com_pw")
+
+
+    if website_credentials != "sudo_let_me_in":
+        logger_main.info("missing/incorrect website_credentials")
+        return make_response('Could not verify', 400, {'message' : 'missing/incorrect website_credentials'})
+    else:
+
+        # TODO: check invite_json_file and apply invites to UsersToRincons Association table
+
+        # open 
+
+        # search for invitations file
+        invitation_json_file_path_and_name = os.path.join(current_app.config.get("DB_ROOT"), "rincon_files","pending_rincon_invitations.json")
+        if os.path.exists(invitation_json_file_path_and_name):
+            invitation_json_file = open(invitation_json_file_path_and_name)
+            invite_dict = json.load(invitation_json_file)
+            invitation_json_file.close()
+
+            list_invite_user_to_delete = []
+            for invited_email, rincon_ids_list in invite_dict.items():
+                invited_user = sess.query(Users).filter_by(email= invited_email).first()
+                if invited_user:
+                    list_invite_user_to_delete.append(invited_email)
+                    for rincon_tuple in rincon_ids_list:
+                        if rincon_tuple[1] == sess.get(Rincons,rincon_tuple[0]).name_no_spaces:
+                            new_rincon_access = UsersToRincons(users_table_id= invited_user.id, rincons_table_id = rincon_tuple[0])
+                            sess.add(new_rincon_access)
+                            sess.commit()
+                    
+            # delete invite user from dict
+            for email in list_invite_user_to_delete:
+                del invite_dict[email]
+            
+
+            # write invite_user_file
+            with open(invitation_json_file_path_and_name,'w') as invitation_json_file:
+                json.dump(invite_dict, invitation_json_file)
+
+
+
+            logger_main.info("Successfully added invites")
+        else:
+            logger_main.info("- No invitation_json_file found")
+
+
+        
+        return jsonify({"status": "Success!"})
 
